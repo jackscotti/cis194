687d9a516cd172367655451fe118922237200404
Jack Scotti <jacopojackscotti@gmail.com>
2017-07-23 20:30:50 +0100

    Do section 3, validate remainder

--- week1.hs
+++ week1.hs
@@ -1,3 +1,4 @@
+-- http://www.seas.upenn.edu/~cis194/spring13/hw/01-intro.pdf
 numbers :: [Integer]
 numbers = [1,2,3,4,5,6,70,8]
 
@@ -23,9 +24,24 @@ separateDigits x
   | x `div` 10 == 0 = [x]
   | otherwise       = separateDigits (x `div` 10) ++ [x `mod` 10]
 
-separateDigitsInList :: [Integer]
-separateDigitsInList = (concat (map separateDigits (doubleEveryOtherReverse numbers)))
+-- separateDigitsInList :: [Integer]
+-- separateDigitsInList = (concat (map separateDigits (doubleEveryOtherReverse numbers)))
 
+separateDigitsInList :: [Integer] -> [Integer]
+separateDigitsInList numbers = (concat (map separateDigits (doubleEveryOtherReverse numbers)))
+
+-- • Calculate the remainder when the sum is divided by 10
+remainder :: [Integer] -> Integer
+remainder numbers = (sum numbers) `mod` 10
+
+isRemainderValid :: Integer -> Bool
+isRemainderValid n
+  | n == 0    = True
+  | otherwise = False
+
+isCreditCardNumberValid :: [Integer] -> Bool
+isCreditCardNumberValid numbers = isRemainderValid (remainder (separateDigitsInList (doubleEveryOther numbers)))
+
+main :: IO()
 main =
-  -- print (doubleEveryOtherReverse numbers)
-  print separateDigitsInList
+  print (isCreditCardNumberValid numbers)
