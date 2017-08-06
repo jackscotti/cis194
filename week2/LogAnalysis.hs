f220c27efc7f12731f15ddc6ba76eadc871b5910
Jack Scotti <jscotti@MacBook-Pro.home>
2017-08-06 22:53:48 +0100

    Finish exercise 1

--- week2/LogAnalysis.hs
+++ week2/LogAnalysis.hs
@@ -16,12 +16,18 @@ mergeMessage = intercalate " "
 -- == LogMessage Info 29 "la la la"
 -- parseMessage "This is not in the right format"
 -- == Unknown "This is not in the right format"
+parseMessage :: String -> LogMessage
+parseMessage message = identifyType $ splitMessage message
+
 identifyType :: [String] -> LogMessage
 identifyType x = case x of
-  "E" : message -> LogMessage (Error (getMessageEntry 0 message)) (getMessageEntry 1 message) (mergeMessage message)
-  "I" : message -> LogMessage Info (getMessageEntry 0 message) (mergeMessage message)
-  "W" : message -> LogMessage Warning (getMessageEntry 0 message) (mergeMessage message)
+  "E" : message -> LogMessage (Error (getMessageEntry 0 message)) (getMessageEntry 1 message) (mergeMessage $ dropNEntries 2 message)
+  "I" : message -> LogMessage Info (getMessageEntry 0 message) (mergeMessage $ dropNEntries 1 message)
+  "W" : message -> LogMessage Warning (getMessageEntry 0 message) (mergeMessage $ dropNEntries 1 message)
   message -> Unknown (mergeMessage message)
 
 getMessageEntry :: Int -> [String] -> Int
 getMessageEntry index message = read (message !! index)
+
+dropNEntries :: Int -> [String] -> [String]
+dropNEntries n message = drop n message
