d8625fbcca08dcbcbb6ed7951f858b9a55086ebc
Jack Scotti <jacopojackscotti@gmail.com>
2017-08-21 19:15:10 +0100

    Ex2

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -1,15 +1,29 @@
 module Golf where
 
-   skips :: [a] -> [[a]]
-   skips []  = []
-   skips [x] = [[x]]
-   skips list@(_:_:rest) = [list, takeEverySecondItem list] ++ convertListIntoListOfLists rest
+  skips :: [a] -> [[a]]
+  skips []  = []
+  skips [x] = [[x]]
+  skips list@(_:_:rest) = [list, takeEverySecondItem list] ++ convertListIntoListOfLists rest
 
-   takeEverySecondItem :: [a] -> [a]
-   takeEverySecondItem [] = []
-   takeEverySecondItem (_:second:remaining) = second : takeEverySecondItem remaining
-   takeEverySecondItem (_:second) = second
+  takeEverySecondItem :: [a] -> [a]
+  takeEverySecondItem [] = []
+  takeEverySecondItem (_:second:remaining) = second : takeEverySecondItem remaining
+  takeEverySecondItem (_:second) = second
 
-   convertListIntoListOfLists :: [a] -> [[a]]
-   convertListIntoListOfLists [] = [[]]
-   convertListIntoListOfLists list =  map (:[]) list
+  convertListIntoListOfLists :: [a] -> [[a]]
+  convertListIntoListOfLists [] = [[]]
+  convertListIntoListOfLists list =  map (:[]) list
+
+
+  localMaxima :: [Integer] -> [Integer]
+  localMaxima [] = []
+  localMaxima [_] = []
+  localMaxima [_,_] = []
+  localMaxima [x,y,z] = [y | isMaxima x y z]
+  localMaxima (x:y:z:remainder) = do
+    let maxima = [y | isMaxima x y z]
+    maxima ++ localMaxima (y:z:remainder)
+
+
+  isMaxima :: Integer -> Integer -> Integer -> Bool
+  isMaxima x y z = x < y && y > z
