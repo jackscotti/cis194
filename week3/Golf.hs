b14111dc337c0129bec7c35c93639fd40577e7f8
Jack Scotti <jacopojackscotti@gmail.com>
2017-08-21 20:17:07 +0100

    Ex 3

--- week3/Golf.hs
+++ week3/Golf.hs
@@ -27,3 +27,23 @@ module Golf where
 
   isMaxima :: Integer -> Integer -> Integer -> Bool
   isMaxima x y z = x < y && y > z
+
+  footer :: String
+  footer = "==========\n0123456789"
+
+  -- buildLine :: [Integer] -> String
+  -- buildLine [] _ = "          "
+  -- buildLine x:xs
+
+  -- buildLineWithString :: [Integer] -> String -> Integer -> String
+  -- buildLineWithString [] row _ = row
+  -- buildLineWithString (x:xs) row counter = do
+  --   let newChar = if x == counter then '*' else ' '
+  --   buildLineWithString xs (row ++ [newChar]) (counter + 1)
+
+  buildLineWithString :: [Integer] -> String -> [Integer] -> String
+  buildLineWithString _ row [] = row
+  buildLineWithString (x:xs) row (y:ys) = do
+    let newChar = if x == y then '*' else ' '
+    buildLineWithString xs (row ++ [newChar]) ys
+  buildLineWithString [] row (_:ys) = buildLineWithString [] (row ++ " ") ys
