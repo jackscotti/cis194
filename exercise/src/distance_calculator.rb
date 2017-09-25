7f67d8ed80885917989a8cd846c746881d0908b6
Jack Scotti <jacopojackscotti@gmail.com>
2017-09-25 14:59:45 +0100

    refactor

--- exercise/src/distance_calculator.rb
+++ exercise/src/distance_calculator.rb
@@ -2,21 +2,20 @@ require 'pry'
 class DistanceCalculator
   OFFICE_COORDINATES = { "latitude" => 53.3393, "longitude" => -6.2576841 }
   EARTH_RADIUS_KM = 6371.0
+  RADIANTS_PER_DEGREE = Math::PI/180
 
   def calculate(loc1, loc2)
-    rad_per_deg = Math::PI/180  # PI / 180
-    earth_radius_in_km = 6371                  # Earth radius in kilometers
 
-    dlat_rad = (loc2[0]-loc1[0]) * rad_per_deg  # Delta, converted to rad
-    dlon_rad = (loc2[1]-loc1[1]) * rad_per_deg
+    dlat_rad = (loc2[0]-loc1[0]) * RADIANTS_PER_DEGREE  # Delta, converted to rad
+    dlon_rad = (loc2[1]-loc1[1]) * RADIANTS_PER_DEGREE
 
-    lat1_rad, lon1_rad = loc1.map {|i| i * rad_per_deg }
-    lat2_rad, lon2_rad = loc2.map {|i| i * rad_per_deg }
+    lat1_rad, lon1_rad = loc1.map {|i| i * RADIANTS_PER_DEGREE }
+    lat2_rad, lon2_rad = loc2.map {|i| i * RADIANTS_PER_DEGREE }
 
     a = Math.sin(dlat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(dlon_rad/2)**2
     c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
 
-    earth_radius_in_km * c # Delta in meters
+    EARTH_RADIUS_KM * c # Delta in meters
   end
   #
   # def distance_2(point1, point2, options = {})
