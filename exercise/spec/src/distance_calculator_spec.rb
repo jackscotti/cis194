c041b3ae4fe8a3bc55a8f5dad5f96891eb07e492
Jack Scotti <jacopojackscotti@gmail.com>
2017-09-25 18:30:46 +0100

    Update customer positio in tests

--- exercise/spec/src/distance_calculator_spec.rb
+++ exercise/spec/src/distance_calculator_spec.rb
@@ -7,7 +7,7 @@ describe DistanceCalculator do
 
   context "calculating distance" do
     it "calculates distance" do
-      customer_position = { "latitude" => 46.3625, "longitude" => 15.114444 }
+      customer_position = { "latitude" => 54.5973, "longitude" => -5.9301 } # Belfast
 
       expect(@calculator.calculate(customer_position)).to eq 1708.4265635304328
     end
