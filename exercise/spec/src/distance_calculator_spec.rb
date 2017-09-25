3f4353bc192348d67af01fbe14254b9816da8d5d
Jack Scotti <jacopojackscotti@gmail.com>
2017-09-25 16:02:24 +0100

    refactor

--- exercise/spec/src/distance_calculator_spec.rb
+++ exercise/spec/src/distance_calculator_spec.rb
@@ -7,10 +7,9 @@ describe DistanceCalculator do
 
   context "calculating distance" do
     it "calculates distance" do
-      point_a = { "latitude" => 46.3625, "longitude" => 15.114444 }
-      point_b = { "latitude" => 46.055556, "longitude" => 14.508333 }
+      customer_position = { "latitude" => 46.3625, "longitude" => 15.114444 }
 
-      expect(@calculator.calculate(point_a, point_b)).to eq 57.794355108740355
+      expect(@calculator.calculate(customer_position)).to eq 1708.4265635304328
     end
   end
 end
--- exercise/src/distance_calculator.rb
+++ exercise/src/distance_calculator.rb
@@ -1,49 +1,32 @@
 require 'pry'
 class DistanceCalculator
-  OFFICE_COORDINATES = { "latitude" => 53.3393, "longitude" => -6.2576841 }
   EARTH_RADIUS_KM = 6371.0
-  RADIANTS_PER_DEGREE = Math::PI/180
+  OFFICE_COORDINATES = {
+    "latitude" => 53.3393,
+    "longitude" => -6.2576841,
+    "latitute_rad" => 0.9309464057090114,
+  }
 
-  def calculate(loc1, loc2)
-    lat_1 = loc1["latitude"]
-    lon_1 = loc1["longitude"]
-    lat_2 = loc2["latitude"]
-    lon_2 = loc2["longitude"]
+  # Formula from http://www.movable-type.co.uk/scripts/latlong.html
+  def calculate(point)
+    point_lat = point["latitude"]
+    point_lon = point["longitude"]
+    point_lat_rad = to_radiants(point_lat)
 
-    delta_lat_rad = to_radiants(lat_2 - lat_1)
-    delta_lon_rad = to_radiants(lon_2 - lon_1)
+    delta_lat_rad = to_radiants(OFFICE_COORDINATES["latitude"]  - point_lat)
+    delta_lon_rad = to_radiants(OFFICE_COORDINATES["longitude"] - point_lon)
 
-    lat1_rad, lon1_rad = to_radiants(lat_1), to_radiants(lon_1)
-    lat2_rad, lon2_rad = to_radiants(lat_2), to_radiants(lon_2)
+    a = (Math.sin(delta_lat_rad / 2))**2 + Math.cos(point_lat_rad) *
+        (Math.sin(delta_lon_rad / 2))**2 * Math.cos(OFFICE_COORDINATES["latitute_rad"])
 
-    a = Math.sin(delta_lat_rad/2)**2 + Math.cos(lat1_rad) * Math.cos(lat2_rad) * Math.sin(delta_lon_rad/2)**2
-    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1-a))
+    c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1 - a))
 
-    EARTH_RADIUS_KM * c # Delta in meters
+    EARTH_RADIUS_KM * c
   end
 
-
-  #
-  # def distance_2(point1, point2, options = {})
-  #   # convert to coordinate arrays
-  #   point1 = extract_coordinates(point1)
-  #   point2 = extract_coordinates(point2)
-  #
-  #   # convert degrees to radians
-  #   point1 = to_radians(point1)
-  #   point2 = to_radians(point2)
-  #
-  #   # compute deltas
-  #   dlat = point2[0] - point1[0]
-  #   dlon = point2[1] - point1[1]
-  #
-  #   a = (Math.sin(dlat / 2))**2 + Math.cos(point1[0]) *
-  #       (Math.sin(dlon / 2))**2 * Math.cos(point2[0])
-  #   c = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
-  #   c * earth_radius
 private
 
   def to_radiants(degrees)
-    degrees * RADIANTS_PER_DEGREE
+    degrees * Math::PI/180
   end
 end
