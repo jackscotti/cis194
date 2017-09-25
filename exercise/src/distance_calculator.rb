5f39d569c22a24d0295570c25c0764b9ba34bd4f
Jack Scotti <jacopojackscotti@gmail.com>
2017-09-25 16:05:10 +0100

    refactor

--- exercise/src/distance_calculator.rb
+++ exercise/src/distance_calculator.rb
@@ -4,20 +4,21 @@ class DistanceCalculator
   OFFICE_COORDINATES = {
     "latitude" => 53.3393,
     "longitude" => -6.2576841,
-    "latitute_rad" => 0.9309464057090114,
   }
 
   # Formula from http://www.movable-type.co.uk/scripts/latlong.html
   def calculate(point)
     point_lat = point["latitude"]
     point_lon = point["longitude"]
-    point_lat_rad = to_radiants(point_lat)
+
+    point_lat_rad  = to_radiants(point_lat)
+    office_lat_rad = to_radiants(OFFICE_COORDINATES["latitude"])
 
     delta_lat_rad = to_radiants(OFFICE_COORDINATES["latitude"]  - point_lat)
     delta_lon_rad = to_radiants(OFFICE_COORDINATES["longitude"] - point_lon)
 
     a = (Math.sin(delta_lat_rad / 2))**2 + Math.cos(point_lat_rad) *
-        (Math.sin(delta_lon_rad / 2))**2 * Math.cos(OFFICE_COORDINATES["latitute_rad"])
+        (Math.sin(delta_lon_rad / 2))**2 * Math.cos(office_lat_rad)
 
     c = 2 * Math::atan2(Math::sqrt(a), Math::sqrt(1 - a))
 
